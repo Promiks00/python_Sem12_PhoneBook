d96996aadd6d83734327dcc32cdf0e966c52baa4
Promiks00 <prodemiks@mail.ru>
2024-02-27 16:31:47 +0300

    added function remove_data();

--- logger.py
+++ logger.py
@@ -46,46 +46,83 @@ def print_data():
 
 def change_data():
     k = int(input("Введите номер записи, который Вы хотите изменить: "))
-    var = input_data()                # Вызываем функцию input_data(), которая добавляет запись в конец файла,
-                                      # а также возвращает значение переменной var
+    var = input_data()  # Вызываем функцию input_data(), которая добавляет запись в конец файла,
+    # а также возвращает значение переменной var
     if var == 1:
-            with open('data_first_variant.csv', 'r', encoding='utf-8') as f:
-                data_first = f.readlines()
-            data_first_list = []
-            j = 0
-            for i in range(len(data_first)):
-                if data_first[i] == '\n' or i == len(data_first) - 1:
-                    data_first_list.append(''.join(data_first[j:i+1]))
-                    j = i
-            data_first_list_final = []
+        with open('data_first_variant.csv', 'r', encoding='utf-8') as f:
+            data_first = f.readlines()
+        data_first_list = []
+        j = 0
+        for i in range(len(data_first)):
+            if data_first[i] == '\n' or i == len(data_first) - 1:
+                data_first_list.append(''.join(data_first[j:i + 1]))
+                j = i
+        data_first_list_final = []
 
-            for i in range(len(data_first_list)):   # Удаляем дублирующиеся переносы строк в списке
-                if data_first_list[i] != '\n\n':
-                    data_first_list_final.append(data_first_list[i])
-            print(data_first_list_final)
+        for i in range(len(data_first_list)):  # Удаляем дублирующиеся переносы строк в списке
+            if data_first_list[i] != '\n\n':
+                data_first_list_final.append(data_first_list[i])
+        print(data_first_list_final)
 
-            change_line = data_first_list_final.pop()   # Извлекаем последнюю запись (элемент) из списка
-                                                        # и сохраняем в переменную change_line
-            data_first_list2 = data_first_list_final[:k - 1] + [change_line] + data_first_list_final[k:]
+        change_line = data_first_list_final.pop()  # Извлекаем последнюю запись (элемент) из списка
+        # и сохраняем в переменную change_line
+        data_first_list2 = data_first_list_final[:k - 1] + [change_line] + data_first_list_final[k:]
 
-
-            with open('data_first_variant.csv', 'w', encoding='utf-8') as f:
-                f.writelines(data_first_list2)
+        with open('data_first_variant.csv', 'w', encoding='utf-8') as f:
+            f.writelines(data_first_list2)
 
     elif var == 2:
         with open('data_second_variant.csv', 'r', encoding='utf-8') as f:
             data_second = f.readlines()
             change_line = data_second.pop()
-            data_second_list = data_second[:k-1] + [change_line] + data_second[k:]
+            data_second_list = data_second[:k - 1] + [change_line] + data_second[k:]
         with open('data_second_variant.csv', 'w', encoding='utf-8') as f:
             f.writelines(data_second_list)
 
 
-change_data()
-def remove_data():
-    pass
+# change_data()
 
-# with open('data_second_variant.csv', 'r+', encoding='utf-8') as f:
-#     data_second = f.readlines()
-#     print(data_second)
+
+def remove_data():
+    var = int(input(f"В каком файле удалить запись?\n\n"
+                    f"1 Вариант (Файл 'data_first_variant.csv'): \n"
+                    f"{'name'}\n{'surname'}\n{'phone'}\n{'address'}\n\n"
+                    f"2 Вариант (Файл 'data_second_variant.csv'): \n"
+                    f"{'name'};{'surname'};{'phone'};{'address'}\n"
+                    f"Выберите вариант: "))
+
+    while var != 1 and var != 2:
+        print("Неправильный ввод")
+        var = int(input("Введите число: "))
+
+    n = int(input("\n Какую запись вы хотите удалить? \n Введите номер записи: "))
+
+    if var == 1:
+        with open('data_first_variant.csv', 'r', encoding='utf-8') as f:
+            data_first = f.readlines()
+            print(data_first)
+            j = 0
+            data_first_list = []
+            for i in range(len(data_first)):
+                if data_first[i] == '\n' or i == len(data_first) - 1:
+                    data_first_list.append(''.join(data_first[j:i + 1]))
+                    j = i
+            data_first_list_final = []
+            for i in range(len(data_first_list)):  # Удаляем дублирующиеся переносы строк в списке
+                if data_first_list[i] != '\n\n':
+                    data_first_list_final.append(data_first_list[i])
+            print(data_first_list_final)
+            data_first_list2 = data_first_list_final[:n - 1] + data_first_list_final[n:]
+            print(data_first_list2)
+        with open('data_first_variant.csv', 'w', encoding='utf-8') as f:
+            f.writelines(data_first_list2)
+    elif var == 2:
+        with open('data_second_variant.csv', 'r', encoding='utf-8') as f:
+            data_second = f.readlines()
+            data_second_list = data_second[:n - 1] + data_second[n:]
+        with open('data_second_variant.csv', 'w', encoding='utf-8') as f:
+            f.writelines(data_second_list)
+
+
+
 
